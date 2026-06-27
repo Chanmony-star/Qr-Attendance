cb2a26ab03da46342bf51cf111b5a840bbda8139
Clarrice332 <vathanamc8@gmail.com>
2026-06-27 12:43:19 +0700

    Query 90% finished, to do: get_all functions for admin dashboard

---
+++ database/query.py
@@ -0,0 +1,50 @@
+from database.models import Student, QrSession, AttendanceRecord
+from django.utils import timezone
+# from database.query import
+def create_student(full_name, student_id):
+    # Register students
+    return Student.objects.create(
+        full_name=full_name,
+        student_id=student_id
+    )
+
+def get_student_by_student_id(student_id):
+    # Search for student by using their student ID
+    return Student.objects.filter(student_id=student_id).first()
+
+def get_valid_session(token):
+    # Search for valid QR sessions by their token
+    session = QrSession.objects.filter(token=token).first()
+
+    if token is None:
+        return None
+    
+    if session.expires_at > timezone.now():
+        return session
+    
+    return None
+
+def get_student_by_token(token):
+    # Search for student by their remember me token
+    return Student.objects.filter(remember_me_token=token)
+
+def save_remember_me_token(student, token):
+    # Save remember me token to a student object
+    # student = get_student_by_id(put in id)
+    student.remember_me_token = token
+    student.save()
+    return student
+
+def already_attended(student, session): # Para are student and session objects
+    # student = get_student_by_student_id()
+    # session = get_valid_session()
+    return AttendanceRecord.objects.filter(
+        student=student,
+        qr_session=session
+    ).exists()
+
+def record_attendance(student, session):
+    return AttendanceRecord.objects.create(
+        student=student,
+        qr_session=session
+    )
